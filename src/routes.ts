2d700d2ab3d56e0115bc356ecf405597754de602
Marcel Dominic Bandowski <marcel.bandowski@dextermedia.net>
2022-11-30 13:59:32 +0100

    Added missing body parser functions

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,28 @@
+import cors from "cors";
 import {Express} from "express";
 import {logger} from "./middleware/logger";
 import {getAllActivitiesController} from "./middleware/controller/activitiesController";
 import {creation} from "./routes/creation";
 
+import pino_http from "pino-http";
+import bodyParser from "body-parser";
+import multer, {Multer} from "multer";
+
+const form: Multer = multer();
+
 export const routes = (app: Express) => {
+    app.use(cors({
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }));
+
+    app.use(pino_http());
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+    app.use(form.any())
+    app.use(creation);
     app.use(creation);
     app.get("/all", getAllActivitiesController)
     app.get("/", (req, res) => res.send('Hello World'));
