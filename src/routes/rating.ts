05265dc16f3e473950c2cc837c9293222437a63f
Marcel Dominic Bandowski <marcel.bandowski@dextermedia.de>
2022-12-05 20:08:08 +0100

    Added space

--- src/routes/rating.ts
+++ src/routes/rating.ts
@@ -1,8 +1,8 @@
-import { Router } from "express";
-import { getAllRatingsController, insertRatingItemController } from "../middleware/controller/ratingController";
+import {Router} from "express";
+import {getAllRatingsController, insertRatingItemController} from "../middleware/controller/ratingController";
 
 export const rating = Router();
 
 rating
     .get("/", getAllRatingsController)
-    .post("/",insertRatingItemController)
+    .post("/", insertRatingItemController)
