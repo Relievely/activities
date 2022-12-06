ccd9e7441f92814cb0b05f61b1aad15944a409f7
Dennis12ax <laa1012@naver.com>
2022-12-06 10:04:08 +0100

    Added test for the View of the information about an activity

--- src/routes/activites.test.ts
+++ src/routes/activites.test.ts
@@ -1,7 +1,7 @@
 import {describe, expect, it, beforeAll} from "@jest/globals";
 import supertest, {Response} from "supertest";
 import {app} from "../app";
-import {RatingItem, ResponseObject} from "../interfaces";
+import {ActivityItem, RatingItem, ResponseObject} from "../interfaces";
 import {RunResult} from "better-sqlite3";
 
 describe("Activities routes", () => {
@@ -49,4 +49,20 @@ describe("Activities routes", () => {
                     });
             });
     })
+
+    const newID: number | bigint = 1;
+
+    it("should return activity item with id", async () => {
+        expect(newID).toBeGreaterThan(0);
+        await requestWithSuperTest
+            .get(`/activity/${newID}`)
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then((response: Response) => {
+                expect(response).toBeDefined();
+                expect((response.body as ResponseObject<ActivityItem>).data).toBeDefined();
+                expect((response.body as ResponseObject<ActivityItem>).data.value).toBeDefined();
+                expect((response.body as ResponseObject<ActivityItem>).data.value.id).toBe(newID);
+            });
+    });
 });
